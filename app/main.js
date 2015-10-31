902f9338dce502d7f197424de07476cbf5654403
Rajiv Mehta <rajiv.mehta@live.in>
2015-10-31 16:34:07 +0530

    add startup code

---
+++ app/main.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="../bower_components/angular/angular.js" />
+/// <reference path="../bower_components/angular-ui-router/release/angular-ui-router.js" />
+
+var avocado = angular.module('avocado', [
+    "ui.router",
+    "avocado.home.controller",
+    'ngMaterial'])
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+        url: '/avocado',
+        templateUrl: 'app/main/partials/avocado.home.tpl.html',
+        controller: 'avocadoHomeController'
+    }).state('home.designer', {
+            url: '/designer',
+            parent: 'home',
+            templateUrl: 'app/main/partials/avocado.home.designer.tpl.html'
+    }).state('home.viewer', {
+        url: '/viewer',
+        parent: 'home',
+        templateUrl: 'app/main/partials/avocado.home.designer.tpl.html'
+    });
+    $urlRouterProvider.otherwise('/', '/avocado/designer')
+}])
---
+++ app/main/controller/avocado.home.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../../../bower_components/angular/angular.js" />
+angular.module('avocado.home.controller', [])
+    .controller('avocadoHomeController', ['$scope', '$state', function ($scope, $state) {
+        (function () {
+            
+        })();
+
+        $scope.onSelectDesigner = function () {
+            $state.go('home.designer');
+        }
+
+        $scope.onSelectViewer = function () {
+            $state.go('home.viewer');
+        }
+
+    }])
